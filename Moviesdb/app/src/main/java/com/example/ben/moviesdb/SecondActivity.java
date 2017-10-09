49e9df92869c8010d7c87d47283a82a02a572d1f
Ikone <elmaildebenja@gmail.com>
2017-10-09 14:27:24 -0700

    Initial commit

---
+++ Moviesdb/app/src/main/java/com/example/ben/moviesdb/SecondActivity.java
@@ -0,0 +1,49 @@
+package com.example.ben.moviesdb;
+        import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.widget.ImageView;
+        import android.widget.TextView;
+
+        import com.squareup.picasso.Picasso;
+
+        import org.w3c.dom.Text;
+
+public class SecondActivity extends AppCompatActivity {
+    public static final String EXTRA_MOVIE = "movie";
+
+    private Movie mMovie;
+    TextView year;
+    ImageView poster;
+    TextView title;
+    TextView description;
+    TextView average;
+    public String year2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.media_delegate);
+        if (getIntent().hasExtra(EXTRA_MOVIE)) {
+            mMovie = getIntent().getParcelableExtra(EXTRA_MOVIE);
+        } else {
+            throw new IllegalArgumentException("Detail activity must receive a movie parcelable");
+        }
+
+        poster = (ImageView) findViewById(R.id.imageView);
+        title = (TextView) findViewById(R.id.movieTittle);
+        description = (TextView) findViewById(R.id.textSummary);
+        year = (TextView) findViewById(R.id.textYear);
+        average = (TextView) findViewById(R.id.textView2);
+
+        title.setText(mMovie.getTitle());
+        description.setText(mMovie.getDescription());
+        year2 = mMovie.getYear();
+        year2 = year2.substring(0,4);
+        year.setText(year2);
+        average.setText(mMovie.getAverage()+ "/10");
+
+        Picasso.with(this)
+                .load(mMovie.getPoster())
+                .into(poster);
+    }
+}
